65786cf345f980731f3690929a854ea0394293e2
dveyronb <71024690+dveyronb@users.noreply.github.com>
2022-08-19 17:24:31 +0400

    коррекция

--- js/script.js
+++ js/script.js
@@ -1,33 +1,20 @@
-let buttonShow = document.querySelector('.button-show');
-let buttonHide = document.querySelector('.button-hide');
-let swiperOpen = document.querySelector('.swiper-container');
-
-if (window.screen.width <= 766);
-{
-  buttonShow.classList.add('button-none');
-  buttonHide.classList.add('button-none');
-}
-if (window.screen.width >= 767);
-{
-  buttonShow.classList.remove('button-none');
-
-  buttonShow.addEventListener('click', function (evt) {
-    buttonHide.classList.remove('button-none');
-    buttonShow.classList.add('button-none');
-    swiperOpen.classList.add('swiper-button-active');
-  });
-
-  buttonHide.addEventListener('click', function (evt) {
-    buttonHide.classList.add('button-none');
-    buttonShow.classList.remove('button-none');
-    swiperOpen.classList.remove('swiper-button-active');
-  });
-}
-
 new Swiper('.brand-slider', {
   pagination: {
     el: '.swiper-pagination',
     //Буллеты
     clickable: true,
   },
+});
+
+let swiperOpen = document.querySelector('.swiper-container');
+let buttonShow = document.querySelector('.button-show');
+
+buttonShow.addEventListener('click', function () {
+  swiperOpen.classList.toggle('swiper-button-active');
+  buttonShow.classList.toggle('button-hide');
+  if (buttonShow.classList.toggle('button-show')) {
+    buttonShow.textContent = 'Показать все';
+  } else {
+    buttonShow.textContent = 'Скрыть';
+  }
 });
